7770de51f6b7ce464ae6d55bd9ba6c32666c90c6
MarcoPasta <Luca.Chiarelli@outlook.de>
2024-03-09 22:21:07 +0100

    fix devcontainer issue & do prettier magic

--- src/components/CardBox/CardsContainer/CardsContainer.tsx
+++ src/components/CardBox/CardsContainer/CardsContainer.tsx
@@ -1,78 +1,91 @@
-import { ReactNode, useEffect, useState } from "react"
-import './CardsContainer.css'
-import { Card, CheckboxState } from "../../../services/interfaces.ts"
-import { FetchCards } from "../../../services/api.ts"
-import { EMPTY_CARD } from "../../../services/constants.ts"
+import { ReactNode, useEffect, useState } from "react";
+import "./CardsContainer.css";
+import { Card, CheckboxState } from "../../../services/interfaces.ts";
+import { FetchCards } from "../../../services/api.ts";
+import { EMPTY_CARD } from "../../../services/constants.ts";
 
 type CardsContainerProps = {
-    checkboxes: CheckboxState,
-    handleCardHover: (item: Card) => void,
-    handlePickedCard: (item: Card) => void
-}
+  checkboxes: CheckboxState;
+  handleCardHover: (item: Card) => void;
+  handlePickedCard: (item: Card) => void;
+};
 
 type ItemProps = {
-    item: Card,
-    checkboxes: CheckboxState,
-    handlePickedCard: (item: Card) => void,
-    handleCardHover: (item: Card) => void
+  item: Card;
+  checkboxes: CheckboxState;
+  handlePickedCard: (item: Card) => void;
+  handleCardHover: (item: Card) => void;
+};
+
+function Item({
+  item,
+  checkboxes,
+  handlePickedCard,
+  handleCardHover,
+}: ItemProps): ReactNode {
+  let key: keyof typeof checkboxes;
+  for (key in checkboxes) {
+    if (item.color === key && checkboxes[key]) {
+      return (
+        <li
+          onClick={() => handlePickedCard(item)}
+          onMouseOver={() => handleCardHover(item)}
+          onMouseLeave={() => handleCardHover(EMPTY_CARD)}
+        >
+          <img src={item.image} alt={item.name} width={120} />
+        </li>
+      );
+    }
+  }
 }
 
-function Item({item, checkboxes, handlePickedCard, handleCardHover}: ItemProps): ReactNode {
-    let key: keyof typeof checkboxes
-    for ( key in checkboxes) {
-        if (item.color === key && checkboxes[key]) {
-            return (
-                <li 
-                    onClick={() => handlePickedCard(item)} 
-                    onMouseOver={() => handleCardHover(item)} 
-                    onMouseLeave={() => handleCardHover(EMPTY_CARD)}
-                >
-                    <img src={item.image} alt={item.name} width={120}/>
-                </li>
-            )
-        }
+export default function CardsContainer({
+  checkboxes,
+  handlePickedCard,
+  handleCardHover,
+}: CardsContainerProps) {
+  // const[url, setUrl] = useState("http://localhost:3000/api/cards/")
+  const url = "http://localhost:3000/api/cards/";
+  // Arrays with retrieved cards
+  const [cards, setCards] = useState<Card[]>([]);
+  // generate random UUID for <li> keys
+
+  async function fetchDataFromApi() {
+    try {
+      setCards([]);
+      const result = await FetchCards(url);
+      setCards(result);
+    } catch (error) {
+      console.error;
     }
-}
+  }
 
-export default function CardsContainer({checkboxes, handlePickedCard, handleCardHover}: CardsContainerProps) {
-    
-    // const[url, setUrl] = useState("http://localhost:3000/api/cards/")
-    const url = "http://localhost:3000/api/cards/"
-    // Arrays with retrieved cards
-    const[cards, setCards] = useState<Card[]>([])
-    // generate random UUID for <li> keys
-    
-    async function fetchDataFromApi() {
-        try {
-            setCards([])
-            const result = await FetchCards(url)
-            setCards(result)
-        } catch (error) {
-            console.error
-        }
-    }
-  
-    useEffect(() => {
-        let ignore = false
-        if (!ignore)
-            fetchDataFromApi()
-  
-        return () => { ignore = true }
-    }, [url])
+  useEffect(() => {
+    let ignore = false;
+    if (!ignore) fetchDataFromApi();
 
+    return () => {
+      ignore = true;
+    };
+  }, [url]);
 
-    return(
-        <>
-            <div className="cardscontainer">
-                <ul>
-                    {cards.map( item => (
-                        
-                        /// TODO: hier schon überprüfen um hohe Calculations zu vermeiden und DANN erst an Item übergeben
+  return (
+    <>
+      <div className="cardscontainer">
+        <ul>
+          {cards.map((item) => (
+            /// TODO: hier schon überprüfen um hohe Calculations zu vermeiden und DANN erst an Item übergeben
 
-                        <Item key={item.id} item={item} checkboxes={checkboxes} handlePickedCard={handlePickedCard} handleCardHover={handleCardHover}/>
-                    ))}
-                </ul>
-            </div>
-        </>
-    )
+            <Item
+              key={item.id}
+              item={item}
+              checkboxes={checkboxes}
+              handlePickedCard={handlePickedCard}
+              handleCardHover={handleCardHover}
+            />
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
